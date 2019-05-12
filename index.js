7c914dd3fec217b87a31d5f733e7749a5cf31371
Azra Kulaglic <akulaglic2@etf.unsa.ba>
2019-05-12 18:03:10 +0200

    Student moze vidjeti obavjestenja predmetnih profesora

--- index.js
+++ index.js
@@ -22,6 +22,36 @@ app.use("/*", (req, res, next) => {
   next();
 });
 
+//Azra
+app.get("/papa/obavjestenjaProfesor", function (req, res) {
+  var id_Studenta = req.body.idStudent;
+
+  db.AkademskaGodina.findOne({where:{aktuelna:{[Op.like]: '1'}}}).then(godina => {
+      db.Korisnik.findAll({attributes :['id'],where:{idUloga:3}}).then(profesor => {
+          niz=[];
+          for(var i = 0; i<profesor.length; i++){
+              niz.push(profesor[i].id);
+          }
+          db.predmet_student.findAll({attributes :['idPredmet'], where: {idStudent: id_Studenta, idAkademskaGodina: godina.id}}).then(veze =>{
+              niz2=[];
+              for(var i = 0; i<veze.length; i++){
+                  niz2.push(veze[i].idPredmet);
+              }
+              db.ObavjestenjePredmet.findAll({where:{idKorisnik:niz, idPredmet:niz2}}).then(obavijest=>{
+                  res.send(obavijest);
+              }).catch(function(err){
+                  console.log({val:err});
+              });
+          }).catch(function(err){
+              console.log({val:err});
+          });           
+      }).catch(function(err){
+          console.log({val:err});
+      });
+  }).catch(function(err){
+      console.log({val:err});
+  });    
+});
 
 
 app.listen(31916, () => console.log('Server na portu: 31916'));
