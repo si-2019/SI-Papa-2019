fe6d7384d97e3396ec84c24b32c02fca2905699b
sademovic <37349892+sademovic@users.noreply.github.com>
2019-06-17 00:45:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/',(req, res)=>{
 });
 
 app.use("/*", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
